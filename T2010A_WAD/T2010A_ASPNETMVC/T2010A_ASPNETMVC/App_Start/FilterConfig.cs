106b7a6aac7f66097afcc4bc393dcc4470a8d147
Tuegtgt11 <73330581+Tuegtgt11@users.noreply.github.com>
2021-11-26 16:53:43 +0700

    Lab 01

---
+++ T2010A_WAD/T2010A_ASPNETMVC/T2010A_ASPNETMVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace T2010A_ASPNETMVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ T2010A_WAD/T2010A_ASPNETMVC/T2010A_ASPNETMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="T2010A_ASPNETMVC.MvcApplication" Language="C#" %>
